586772043eaadbebbdfef941834d63c89685b9d6
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-02-09 12:53:49 +0530

    Create variables.tf

---
+++ storage-account/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  type    = string
+  default = "azure"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "account_tier" {
+  type    = string
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  type    = string
+  default = "GRS"
+}
