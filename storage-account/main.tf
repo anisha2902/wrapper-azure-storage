9edf9011eba545d774debd849b9b9074c19841b2
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-02-09 13:40:46 +0530

    Update main.tf

--- storage-account/main.tf
+++ storage-account/main.tf
@@ -1,6 +1,6 @@
 module "azure_storage" {
   source = "git::https://github.com/anisha2902/module-azure-storage.git//storage-account?ref=main"
-  name       = var.vm_name
+  name       = var.name
   location = var.location
   account_tier = var.account_tier
   account_replication_type = var.account_replication_type
